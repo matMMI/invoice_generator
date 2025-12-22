83efb4a56629f611eae5fd8e03ee1b4ce0ce8e4c
mthtgi <mthtgi@mac.home>
2025-12-22 22:22:35 +0100

    test: Remove rewrites to test Vercel auto-detection of Python functions

--- next.config.ts
+++ next.config.ts
@@ -3,14 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: "/api/index.py",
-      },
-    ];
-  },
 };
 
 export default nextConfig;
